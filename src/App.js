17058206a6c203b58f52a8cac3791c47f890660f
Fanare <nissan200oe@yandex.ru>
2023-07-12 19:03:24 +0300

    RandomeQuote realisation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App() {  
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container-fluid">
+  <h1 className='text-primary'>Random Quotes!</h1>
+  
+  <div className="well">
+    
+    <p className="quote-text">{quotes.text}</p>
+    <p className="author-text">{quotes.author}</p>
+  </div>
+  <button type="submit" className="btn btn-primary" id="quote" onClick={getQuote}>New Quote</button>
+</section>
   );
 }
 
